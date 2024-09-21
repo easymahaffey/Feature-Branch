fc70ef2942aa61e78896fc4b49cbec6b62cd72b8
Easy Mahaffey <easymahaffey90@gmail.com>
2024-09-21 14:57:55 -0700

    original hello world message

--- main.py
+++ main.py
@@ -2,5 +2,9 @@
 def hello_world():
     return "Hello from Easy!"
 
+def greet_person():
+    return "Hello, Easy!"
+
 if __name__ == "__main__":
     print(hello_world())
+    print(greet_person())
