8b32eb79cb618f4a36f6e266ee256d84c145fd93
Easy Mahaffey <easymahaffey90@gmail.com>
2024-09-21 14:36:33 -0700

    second commit

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ from main import hello_world
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(hello_world(), "Hello, Git World!")
+        self.assertEqual(hello_world(), "Hello from Easy!")
 
 if __name__ == '__main__':
     unittest.main()
